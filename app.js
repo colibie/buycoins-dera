7d445e2c4bb396428df3d8173d07371319540775
Chidera Olibie <jenniferolibie@gmail.com>
2020-02-26 02:44:00 +0100

    added intro message

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const {schema} = require('./schema');
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use('/', (req, res) => {
+    res.json({
+        status: 200,
+        message: `Hiiii, please navigate to /graphql for the interface. The format of the graphql query is { calculatePrice(type: "buy", margin: 0.2, exchangeRate: 360.0) }`
+    })
+})
+
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
