177795c7088cae7ec94b617def83edcb98e4a718
Chidera Olibie <jenniferolibie@gmail.com>
2020-02-26 02:56:24 +0100

    irrevelant changes

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ const {schema} = require('./schema');
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use('/', (req, res) => {
-    res.json({
-        status: 200,
-        message: `Hiiii, please navigate to /graphql for the interface. The format of the graphql query is { calculatePrice(type: "buy", margin: 0.2, exchangeRate: 360.0) }`
-    })
-})
-
-app.use('/graphql', graphqlHTTP({
+app.use('/graphiql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true,
 }));
 
-app.listen(port, () => console.log('Now browse to localhost:4000/graphql'));
+app.use('/', (req, res) => {
+    res.json({
+        status: 200,
+        message: `Hiiii, please navigate to /graphiql for the interface. The format of the graphql query is { calculatePrice(type: "buy", margin: 0.2, exchangeRate: 360.0) }`
+    })
+})
+
+app.listen(port, () => console.log('Now browse to localhost:4000/graphiql'));
